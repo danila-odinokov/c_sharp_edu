88479f9f27d6803063e6b84a3d015f2a105cb16e
Danila <danila_odinokov@outlook.com>
2022-08-07 16:10:45 +0300

    domashka

--- SEMINAR_1/sem_1_8/Program.cs
+++ SEMINAR_1/sem_1_8/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+int b = 0;
+
+while (b < a-1)
+{
+    b = b + 2;
+    Console.Write(b + " ");
+}
