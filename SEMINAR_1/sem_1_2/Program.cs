b83c06d815fd42fb99e2c293068e1972a0db8c04
Danila <danila_odinokov@outlook.com>
2022-08-07 15:54:36 +0300

    restore

---
+++ SEMINAR_1/sem_1_2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число 1");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+int b = int.Parse(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine($"Число {a} больше, чем {b}");
+}
+else
+{
+    Console.WriteLine($"Число {b} больше, чем {a}");  
+}
---
+++ SEMINAR_1/sem_1_4/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число 1");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число 3");
+int c = int.Parse(Console.ReadLine());
+
+if (a > b & a > c)
+{
+    Console.WriteLine($"Число {a} наибольшее");
+}
+else if (b > c)
+{
+    Console.WriteLine($"Число {b} наибольшее");  
+}
+else
+{
+    Console.WriteLine($"Число {c} наибольшее");  
+}
