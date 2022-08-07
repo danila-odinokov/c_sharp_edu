4f3ddb228990d9dc6a35d4d21494f2fe9ad68649
Danila <danila_odinokov@outlook.com>
2022-08-07 16:12:02 +0300

    domashka 2

--- SEMINAR_1/sem_1_6/Program.cs
+++ SEMINAR_1/sem_1_6/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+a = (a % 10) % 2;
+
+if (a==0)
+    {
+        Console.WriteLine("Число чётное");
+    }
+else
+    {
+        Console.WriteLine("Число нечётное");
+    }   
+    
