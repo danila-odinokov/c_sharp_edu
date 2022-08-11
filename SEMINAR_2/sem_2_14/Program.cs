d72c86697aa74a2020bf2a7f22580a9b3ff38c98
Danila <danila_odinokov@outlook.com>
2022-08-11 18:33:03 +0300

    seminar 2 second half

---
+++ SEMINAR_2/sem_2_14/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+
+if (a%7==0 && a%23==0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
---
+++ SEMINAR_2/sem_2_16/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число 1");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+int b = int.Parse(Console.ReadLine());
+
+if(Math.Pow(b,2) == a)
+{
+    Console.WriteLine("b квадрат a");
+}
+if(Math.Pow(a,2) == b)
+{
+    Console.WriteLine("a квадрат b");
+}
+else
+{
+    Console.WriteLine("не является");
+}
