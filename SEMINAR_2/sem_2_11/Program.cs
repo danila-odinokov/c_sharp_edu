24d98b5a40add0fdf414b60906372ab113ae6ea3
Danila <danila_odinokov@outlook.com>
2022-08-11 16:37:50 +0300

    seminar 2 half

---
+++ SEMINAR_2/sem_2_11/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+
+int a = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число {a}");
+int first = (a / 100)*10; 
+int third = a%10;
+int result = first+third;
+
+Console.WriteLine($"Результат {result}");
---
+++ SEMINAR_2/sem_2_12/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число 1");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+int b = int.Parse(Console.ReadLine());
+
+if (a%b == 0)
+{
+    Console.WriteLine($"Число {a} кратно числу {b}");   
+}
+else 
+{
+    Console.WriteLine($"Число {a} не кратно числу {b}\nОстаток {a%b}");
+}
