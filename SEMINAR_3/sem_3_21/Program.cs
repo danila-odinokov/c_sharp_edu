77a43326ed0768384024a7ea7bf1640e465328ee
Danila <danila_odinokov@outlook.com>
2022-08-17 14:18:05 +0300

    lec 3

--- SEMINAR_3/sem_3_21/Program.cs
+++ SEMINAR_3/sem_3_21/Program.cs
@@ -4,14 +4,17 @@ Console.WriteLine("Введите число x1:");
 double x1 = double.Parse(Console.ReadLine());
 Console.WriteLine("Введите число y1:");
 double y1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите число z1:");
+double z1 = double.Parse(Console.ReadLine());
 
 Console.WriteLine("Введите число x2:");
 double x2 = double.Parse(Console.ReadLine());
 Console.WriteLine("Введите число y2:");
 double y2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите число z2:");
+double z2 = double.Parse(Console.ReadLine());
 
-double a = Math.Pow(x1 - x2,2);
-double b = Math.Pow(y1 - y2,2);
-double c = Math.Sqrt(a+b);
 
-Console.WriteLine($"Расстояние между точками {c:f3}");
+double d = Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2)+Math.Pow(z2-z1,2));
+
+Console.WriteLine($"Расстояние между точками {d:f3}");
---
+++ SEMINAR_3/sem_3_23/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число");
+int x = int.Parse(Console.ReadLine());
+int y = 1;
+
+while (y <= x)
+{
+    Console.Write(Math.Pow(y,3) + " ");
+    y++;
+}
